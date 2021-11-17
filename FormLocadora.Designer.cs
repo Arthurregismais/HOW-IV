6f5e09c8f910857249a523dde479a55a3c9f1966
Arthurregismais <89051127+Arthurregismais@users.noreply.github.com>
2021-11-17 10:59:50 -0300

    Add files via upload
    Professor, o packages vou colocar em uma pasta separada porque o git me disse que eram muitos arquivos

---
+++ FormLocadora.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace LocadoraDeCarros
+{
+    partial class FormLocadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLocadora));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbMarca = new System.Windows.Forms.TextBox();
+            this.tbDescrição = new System.Windows.Forms.TextBox();
+            this.tbAno = new System.Windows.Forms.TextBox();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btInserir = new System.Windows.Forms.Button();
+            this.dgLocadoraDeCarros = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMarca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescrição = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.btn_TrocarUsuario = new System.Windows.Forms.Button();
+            this.btn_GestaoClientes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLocadoraDeCarros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marca";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descrição";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ano";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(134, 16);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(100, 20);
+            this.tbNome.TabIndex = 4;
+            // 
+            // tbMarca
+            // 
+            this.tbMarca.Location = new System.Drawing.Point(301, 47);
+            this.tbMarca.Name = "tbMarca";
+            this.tbMarca.Size = new System.Drawing.Size(100, 20);
+            this.tbMarca.TabIndex = 5;
+            // 
+            // tbDescrição
+            // 
+            this.tbDescrição.Location = new System.Drawing.Point(301, 16);
+            this.tbDescrição.Name = "tbDescrição";
+            this.tbDescrição.Size = new System.Drawing.Size(487, 20);
+            this.tbDescrição.TabIndex = 6;
+            // 
+            // tbAno
+            // 
+            this.tbAno.Location = new System.Drawing.Point(134, 47);
+            this.tbAno.Name = "tbAno";
+            this.tbAno.Size = new System.Drawing.Size(100, 20);
+            this.tbAno.TabIndex = 7;
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(436, 45);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpar.TabIndex = 8;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // btInserir
+            // 
+            this.btInserir.Location = new System.Drawing.Point(517, 45);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.TabIndex = 9;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.UseVisualStyleBackColor = true;
+            this.btInserir.Click += new System.EventHandler(this.btInserir_Click);
+            // 
+            // dgLocadoraDeCarros
+            // 
+            this.dgLocadoraDeCarros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLocadoraDeCarros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colNome,
+            this.colMarca,
+            this.colDescrição,
+            this.colAno});
+            this.dgLocadoraDeCarros.Location = new System.Drawing.Point(25, 165);
+            this.dgLocadoraDeCarros.Name = "dgLocadoraDeCarros";
+            this.dgLocadoraDeCarros.Size = new System.Drawing.Size(763, 257);
+            this.dgLocadoraDeCarros.TabIndex = 10;
+            this.dgLocadoraDeCarros.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgLocadoraDeCarros_CellContentClick);
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.Name = "colID";
+            this.colID.Width = 30;
+            // 
+            // colNome
+            // 
+            this.colNome.HeaderText = "Nome";
+            this.colNome.Name = "colNome";
+            this.colNome.Width = 170;
+            // 
+            // colMarca
+            // 
+            this.colMarca.HeaderText = "Marca";
+            this.colMarca.Name = "colMarca";
+            this.colMarca.Width = 170;
+            // 
+            // colDescrição
+            // 
+            this.colDescrição.HeaderText = "Descrição";
+            this.colDescrição.Name = "colDescrição";
+            this.colDescrição.Width = 300;
+            // 
+            // colAno
+            // 
+            this.colAno.HeaderText = "Ano";
+            this.colAno.Name = "colAno";
+            this.colAno.Width = 50;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(436, 74);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 11;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(517, 74);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 12;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ID";
+            // 
+            // tbID
+            // 
+            this.tbID.Enabled = false;
+            this.tbID.Location = new System.Drawing.Point(46, 16);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(40, 20);
+            this.tbID.TabIndex = 14;
+            // 
+            // btn_TrocarUsuario
+            // 
+            this.btn_TrocarUsuario.Location = new System.Drawing.Point(436, 103);
+            this.btn_TrocarUsuario.Name = "btn_TrocarUsuario";
+            this.btn_TrocarUsuario.Size = new System.Drawing.Size(156, 23);
+            this.btn_TrocarUsuario.TabIndex = 15;
+            this.btn_TrocarUsuario.Text = "Trocar Usuário";
+            this.btn_TrocarUsuario.UseVisualStyleBackColor = true;
+            this.btn_TrocarUsuario.Click += new System.EventHandler(this.btn_TrocarUsuario_Click);
+            // 
+            // btn_GestaoClientes
+            // 
+            this.btn_GestaoClientes.Location = new System.Drawing.Point(436, 132);
+            this.btn_GestaoClientes.Name = "btn_GestaoClientes";
+            this.btn_GestaoClientes.Size = new System.Drawing.Size(156, 23);
+            this.btn_GestaoClientes.TabIndex = 16;
+            this.btn_GestaoClientes.Text = "Gestão de Clientes";
+            this.btn_GestaoClientes.UseVisualStyleBackColor = true;
+            this.btn_GestaoClientes.Click += new System.EventHandler(this.btn_GestaoClientes_Click);
+            // 
+            // FormLocadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_GestaoClientes);
+            this.Controls.Add(this.btn_TrocarUsuario);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.dgLocadoraDeCarros);
+            this.Controls.Add(this.btInserir);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.tbAno);
+            this.Controls.Add(this.tbDescrição);
+            this.Controls.Add(this.tbMarca);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLocadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Locadora de Carros";
+            this.Load += new System.EventHandler(this.FormLocadora_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgLocadoraDeCarros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbMarca;
+        private System.Windows.Forms.TextBox tbDescrição;
+        private System.Windows.Forms.TextBox tbAno;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btInserir;
+        private System.Windows.Forms.DataGridView dgLocadoraDeCarros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMarca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescrição;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAno;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Button btn_TrocarUsuario;
+        private System.Windows.Forms.Button btn_GestaoClientes;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using LocadoraDeCarros;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+
+
+
+namespace LocadoraDeCarros
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
